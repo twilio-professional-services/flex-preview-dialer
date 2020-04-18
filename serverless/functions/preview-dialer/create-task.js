76a2e09570fce7c06a23cf9992660b77a8bb59cb
Kaique Lupo <klupo@twilio.com>
2020-04-18 11:10:51 -0300

    change null case for schedule attribute

--- serverless/functions/preview-dialer/create-task.js
+++ serverless/functions/preview-dialer/create-task.js
@@ -3,12 +3,20 @@ const TokenValidator = require('twilio-flex-token-validator').functionValidator;
 let path = Runtime.getFunctions()['utils'].path;
 let assets = require(path);
 
+const DAYS = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+
 exports.handler = TokenValidator( async (context, event, callback) => {
 
   const client = context.getTwilioClient();
 
   const contacts = (event.contacts && JSON.parse(event.contacts)) || [];
-  const schedule = (event.schedule && JSON.parse(event.schedule)) || null;
+  const schedule = 
+    (event.schedule && JSON.parse(event.schedule)) || 
+    ({
+      week: DAYS,
+      startHour: "0000",
+      endHour: "2359",
+    });
 
   const { campaign } = event;
 
